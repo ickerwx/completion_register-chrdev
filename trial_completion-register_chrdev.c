45218c576528a515d6d1e691f07ef0a44e9166e0
Tom Xue <tomxue026@gmail.com>
2012-01-16 07:21:07 +0000

    tidy design

--- trial_completion-register_chrdev.c
+++ trial_completion-register_chrdev.c
@@ -14,18 +14,15 @@ DECLARE_COMPLETION(comp);
 
 ssize_t complete_read (struct file *filp, char __user *buf, size_t count, loff_t *pos)
 {
-         printk(KERN_DEBUG "process %i (%s) going to sleep\n",
-         current->pid, current->comm);
+         printk(KERN_DEBUG "process %i (%s) going to sleep\n", current->pid, current->comm);
          wait_for_completion(&comp);
          printk(KERN_DEBUG "awoken %i (%s)\n", current->pid, current->comm);
          return 0; /* EOF */
 }
 
-ssize_t complete_write (struct file *filp, const char __user *buf, size_t count,
-    loff_t *pos)
+ssize_t complete_write (struct file *filp, const char __user *buf, size_t count, loff_t *pos)
 {
-         printk(KERN_DEBUG "process %i (%s) awakening the readers...\n",
-         current->pid, current->comm);
+         printk(KERN_DEBUG "process %i (%s) awakening the readers...\n", current->pid, current->comm);
          complete(&comp);
          return count; /* succeed, to avoid retrial */
 }
@@ -33,16 +30,15 @@ ssize_t complete_write (struct file *filp, const char __user *buf, size_t count,
 
 struct file_operations complete_fops = {
          .owner = THIS_MODULE,
-         .read =    complete_read,
+         .read = complete_read,
          .write = complete_write,
 };
 
 
 int complete_init(void)
 {
-         int result;
-
-/*
+       int result;
+   /*
     * Register your major, and accept a dynamic number
     */
         result = register_chrdev(complete_major, "complete", &complete_fops);
